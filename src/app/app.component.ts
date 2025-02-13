dcfaeb73c306abcb20541ffbebf918e55d7f4855
HaiquanZ <nn764562@gmail.com>
2025-02-13 22:54:24 +0700

    [OCR] Use Tesseract for OCR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Tesseract from 'tesseract.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'store';
+
+  recognizedText: string = '';
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      Tesseract.recognize(file as any, 'eng', {
+        logger: (m: any) => console.log(m)
+      }).then((result: { data: { text: string } }) => {
+        this.recognizedText = result.data.text;
+      });
+    }
+  }
 }
